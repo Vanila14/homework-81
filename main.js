216f97808c27e743aff915e6a0058987b74bf91e
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2023-09-03 00:20:06 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const rainbow =['red', 'orange', 'yellow', 'green','blue','Indigo','Violet']
 for (let i = 1; i < numbers.length; i = i+2 ){
     console.log(numbers[i])
 }
-for (let r = 6; r < rainbow.length; r = r-1 ){
-    console.log(rainbow[r])
-}    
+// for (let r = 6; r < rainbow.length; r = r-1 ){
+//     console.log(rainbow[r])
+// }    
 
 
