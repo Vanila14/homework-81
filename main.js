d3e8489304b5f7dbcdb675208026d42b63600238
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2023-09-03 21:32:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]
 const rainbow =['red', 'orange', 'yellow', 'green','blue','Indigo','Violet']
 
-for (let i = 1; i < numbers.length; i = i+2 ){
-    console.log(numbers[i])
+for (var i = 0; i < numbers.length; i++)
+{
+    if ((numbers[i] % 2) === 0)
+{
+console.log(numbers[i]);
+}
 }
 for (let i = rainbow.length - 1; i >= 0; i--){
     console.log(rainbow[i])
